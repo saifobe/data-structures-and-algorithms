cd147f4f8a58e9cf3d706b18c914ce96bdf4a44b
saifobe <obeidatsaif93@gmail.com>
2023-04-03 19:12:06 +0000

    code challenge 01 array reverse done

---
+++ reverseArray.py
@@ -0,0 +1,10 @@
+def reverseArray(arr):
+    
+    n = len(arr)
+    for i in range(n // 2):
+        arr[i], arr[n - i - 1] = arr[n - i - 1], arr[i]
+    return arr
+
+
+my_arr = [1, 2, 3, 4, 5]
+print(reverseArray(my_arr))
