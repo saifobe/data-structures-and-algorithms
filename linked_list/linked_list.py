d843008e91f3bc3c0c296f319647cf8e703db004
saifobe <obeidatsaif93@gmail.com>
2023-04-17 00:49:07 +0000

    code challenge 06 done

---
+++ linked_list/linked_list.py
@@ -0,0 +1,184 @@
+from linked_list.node import Node
+
+class LinkedList:
+
+    """
+    A class representing a linked list, a data structure made up of a sequence of nodes, each containing some data
+    and a reference to the next node in the sequence.
+
+    Attributes:
+    ----------
+    head : Node or None
+        The first node in the linked list, or None if the list is empty.
+    """
+    
+    def __init__(self):
+        self.head = None
+
+    def insert(self, value):
+        """
+        Inserts a new node containing the given value at the beginning of the linked list.
+
+        Parameters:
+        ----------
+        value : any
+            The value to be stored in the new node.
+
+        Returns:
+        -------
+        None
+        """
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
+
+    def append(self, value):
+        """
+        Adds a new node containing the given value at the end of the linked list.
+
+        Parameters:
+        ----------
+        value : any
+            The value to be stored in the new node.
+
+        Returns:
+        -------
+        None
+        """
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            return
+        current_node = self.head
+        while current_node.next:
+            current_node = current_node.next
+        current_node.next = new_node
+
+    def insert_before(self, value, new_value):
+        """
+        Inserts a new node containing the given new_value before the first occurrence of the given value in the linked list.
+
+        Parameters:
+        ----------
+        value : any
+            The value to search for in the linked list.
+        new_value : any
+            The value to be stored in the new node.
+
+        Returns:
+        -------
+        None
+        """
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            return
+        if self.head.value == value:
+            new_node.next = self.head
+            self.head = new_node
+            return
+        current_node = self.head
+        while current_node.next:
+            if current_node.next.value == value:
+                new_node.next = current_node.next
+                current_node.next = new_node
+                return
+            current_node = current_node.next
+
+    def insert_after(self, value, new_value):
+        """
+        Inserts a new node containing the given new_value after the first occurrence of the given value in the linked list.
+
+        Parameters:
+        ----------
+        value : any
+            The value to search for in the linked list.
+        new_value : any
+            The value to be stored in the new node.
+
+        Returns:
+        -------
+        None
+        """
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            return
+        current_node = self.head
+        while current_node:
+            if current_node.value == value:
+                new_node.next = current_node.next
+                current_node.next = new_node
+                return
+            current_node = current_node.next
+
+    def includes(self, value):
+        """
+        Inserts a new node containing the given new_value after the first occurrence of the given value in the linked list.
+
+        Parameters:
+        ----------
+        value : any
+            The value to search for in the linked list.
+        new_value : any
+            The value to be stored in the new node.
+
+        Returns:
+        -------
+        None
+        """
+        current_node = self.head
+        while current_node:
+            if current_node.value == value:
+                return True
+            current_node = current_node.next
+        return False
+
+    def to_string(self):
+        """
+        Returns a string representation of the linked list, with each node's value enclosed in curly braces and
+        separated by arrow symbols pointing to the next node. The last arrow points to NULL to indicate the end of the
+        list.
+
+        Returns:
+        -------
+        str
+            A string representation of the linked list.
+        """
+        current_node = self.head
+        result = ""
+        while current_node:
+            result += "{ " + str(current_node.value) + " } -> "
+            current_node = current_node.next
+        result += "NULL"
+        return result
+
+    def delete(self, value):
+        """
+        Deletes the first node containing the given value from the linked list, if it exists.
+
+        Parameters:
+        ----------
+        value : any
+            The value to search for and delete from the linked list.
+
+        Returns:
+        -------
+        None
+        """
+        current_node = self.head
+        if current_node and current_node.value == value:
+            self.head = current_node.next
+            current_node = None
+            return
+
+        previous_node = None
+        while current_node and current_node.value != value:
+            previous_node = current_node
+            current_node = current_node.next
+
+        if current_node is None:
+            return
+
+        previous_node.next = current_node.next
+        current_node = None
---
+++ linked_list/tests/test_linkedList.py
@@ -0,0 +1,104 @@
+import pytest
+from linked_list.linked_list import LinkedList
+from linked_list.node import Node
+
+
+def test_empty_linked_list():
+    linked_list = LinkedList()
+    assert linked_list.head == None
+
+
+def test_insert_linked_list():
+    linked_list = LinkedList()
+    linked_list.insert(5)
+    assert linked_list.head.value == 5
+
+
+def test_insert_multiple_linked_list():
+    linked_list = LinkedList()
+    linked_list.insert(5)
+    linked_list.insert(10)
+    linked_list.insert(15)
+    assert linked_list.head.value == 15
+    assert linked_list.head.next.value == 10
+    assert linked_list.head.next.next.value == 5
+
+
+def test_find_existing_value():
+    linked_list = LinkedList()
+    linked_list.insert(5)
+    linked_list.insert(10)
+    linked_list.insert(15)
+    assert linked_list.includes(10) == True
+
+
+def test_find_non_existing_value():
+    linked_list = LinkedList()
+    linked_list.insert(5)
+    linked_list.insert(10)
+    linked_list.insert(15)
+    assert linked_list.includes(20) == False
+
+
+def test_to_string():
+    linked_list = LinkedList()
+    linked_list.insert(5)
+    linked_list.insert(10)
+    linked_list.insert(15)
+    assert linked_list.to_string() == "{ 15 } -> { 10 } -> { 5 } -> NULL"
+
+
+def test_append_linked_list():
+    linked_list = LinkedList()
+    linked_list.append(5)
+    assert linked_list.head.value == 5
+
+
+def test_append_multiple_linked_list():
+    linked_list = LinkedList()
+    linked_list.append(5)
+    linked_list.append(10)
+    linked_list.append(15)
+    assert linked_list.head.value == 5
+    assert linked_list.head.next.value == 10
+    assert linked_list.head.next.next.value == 15
+
+
+def test_insert_before_linked_list_middle():
+    linked_list = LinkedList()
+    linked_list.insert(5)
+    linked_list.insert(10)
+    linked_list.insert(15)
+    linked_list.insert_before(10, 20)
+    assert linked_list.to_string(
+    ) == "{ 15 } -> { 20 } -> { 10 } -> { 5 } -> NULL"
+
+
+def test_insert_before_linked_list_first():
+    linked_list = LinkedList()
+    linked_list.insert(5)
+    linked_list.insert(10)
+    linked_list.insert(15)
+    linked_list.insert_before(15, 20)
+    assert linked_list.to_string(
+    ) == "{ 20 } -> { 15 } -> { 10 } -> { 5 } -> NULL"
+
+
+def test_insert_after_linked_list_middle():
+    linked_list = LinkedList()
+    linked_list.insert(5)
+    linked_list.insert(10)
+    linked_list.insert(15)
+    linked_list.insert_after(10, 20)
+    assert linked_list.to_string(
+    ) == "{ 15 } -> { 10 } -> { 20 } -> { 5 } -> NULL"
+
+
+def test_insert_after_linked_list_last():
+    linked_list = LinkedList()
+    linked_list.insert(5)
+    linked_list.insert(10)
+    linked_list.insert(15)
+    linked_list.insert_after(5, 20)
+    assert linked_list.to_string(
+    ) == "{ 15 } -> { 10 } -> { 5 } -> { 20 } -> NULL"
